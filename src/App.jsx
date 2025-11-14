5f1a3d4285e39d18fe13a7257961689dd9046488
CarloMart88 <carlo.martino1988@gmail.com>
2025-11-14 13:20:23 +0100

    add  boostrap

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import { useState } from "react";
 
 function App() {
   return (
-    <>
-      <div className="color">
-        <h1>Repo: ex-react-usereducer-spesa</h1>
+    <div className="container">
+      <div className="row d-flex">
+        <div className="col-6">
+          <h1>Repo: ex-react-usereducer-spesa</h1>
+        </div>
+        <div className="col-6">eccone due</div>
       </div>
-    </>
+    </div>
   );
 }
 
